870ec1a919613091a2f3d357e1e2853c78a1d021
Youssr <cyoussr@gmail.com>
2024-05-31 20:06:59 +0100

    commited

--- MRXSTORE.js
+++ MRXSTORE.js
@@ -1,10 +1,12 @@
 let videos = Array.from(document.querySelectorAll('#videoSlideshow video'));
-videos[0].classList.add('current'); // Start with the first video
+videos[0].classList.add('current'); 
+videos[0].play();
 
 videos.forEach((video, index) => {
     video.onended = () => {
         video.classList.remove('current');
         let nextVideo = videos[(index + 1) % videos.length]; // Loop back to the first video after the last one
         nextVideo.classList.add('current');
+        nextVideo.play();
     };
 });
